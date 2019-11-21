1bf0241022d54ab39c995882836305f5e48ea405
Mitch Tabian <mitch@tabian.ca>
2019-11-20 17:29:21 -0800

    Activity Navigation

--- app/src/androidTest/java/com/codingwithmitch/espressouitestexamples/ui/main/MainActivityTest.kt
+++ app/src/androidTest/java/com/codingwithmitch/espressouitestexamples/ui/main/MainActivityTest.kt
@@ -1,12 +1,7 @@
 package com.codingwithmitch.espressouitestexamples.ui.main
 
-import android.view.View
 import androidx.test.core.app.ActivityScenario
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
-import com.codingwithmitch.espressouitestexamples.R
 import org.junit.Test
 
 import org.junit.runner.RunWith
@@ -14,37 +9,13 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4ClassRunner::class)
 class MainActivityTest{
 
+
     @Test
-    fun testActivity_inView() {
+    fun test_navSecondaryActivity() {
         val activityScenario = ActivityScenario.launch(MainActivity::class.java)
 
-        onView(withId(R.id.main)).check(matches(isDisplayed()))
+
     }
-
-    // Visibility
-    @Test
-    fun testVisibility_title_nextButton() {
-        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
-
-        onView(withId(R.id.activity_main_title))
-            .check(matches(isDisplayed())) // method 1
-
-        onView(withId(R.id.activity_main_title))
-            .check(matches(withEffectiveVisibility(Visibility.VISIBLE))) // method 2
-
-        onView(withId(R.id.button_next_activity))
-            .check(matches(isDisplayed()))
-    }
-
-    // Text
-    @Test
-    fun testTitleTextDisplayed() {
-        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
-
-        onView(withId(R.id.activity_main_title))
-            .check(matches(withText(R.string.text_mainactivity)))
-    }
-
 }
 
 
