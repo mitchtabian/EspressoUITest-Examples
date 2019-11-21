177c9e1e95fd53a2893899f942d9f480ce183686
Mitch Tabian <mitch@tabian.ca>
2019-11-20 18:14:01 -0800

    simple activity navigation tests

--- app/src/androidTest/java/com/codingwithmitch/espressouitestexamples/ui/main/MainActivityTest.kt
+++ app/src/androidTest/java/com/codingwithmitch/espressouitestexamples/ui/main/MainActivityTest.kt
@@ -1,7 +1,14 @@
 package com.codingwithmitch.espressouitestexamples.ui.main
 
 import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.pressBack
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import com.codingwithmitch.espressouitestexamples.R
 import org.junit.Test
 
 import org.junit.runner.RunWith
@@ -9,12 +16,37 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4ClassRunner::class)
 class MainActivityTest{
 
-
     @Test
     fun test_navSecondaryActivity() {
+
         val activityScenario = ActivityScenario.launch(MainActivity::class.java)
 
+        onView(withId(R.id.button_next_activity)).perform(click())
 
+        onView(withId(R.id.secondary)).check(matches(isDisplayed()))
+    }
+
+    /**
+     * Test both ways to navigate from SecondaryActivity to MainActivity
+     */
+    @Test
+    fun test_backPress_toMainActivity() {
+
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+
+        onView(withId(R.id.button_next_activity)).perform(click())
+
+        onView(withId(R.id.secondary)).check(matches(isDisplayed()))
+
+        onView(withId(R.id.button_back)).perform(click()) // method 1
+
+        onView(withId(R.id.main)).check(matches(isDisplayed()))
+
+        onView(withId(R.id.button_next_activity)).perform(click())
+
+        pressBack() // method 2
+
+        onView(withId(R.id.main)).check(matches(isDisplayed()))
     }
 }
 
