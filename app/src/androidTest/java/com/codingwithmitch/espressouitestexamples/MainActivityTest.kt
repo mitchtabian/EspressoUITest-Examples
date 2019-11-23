b73be337252e14dbfb78b45d0f05ad2e85d5aa23
Mitch Tabian <mitch@tabian.ca>
2019-11-22 18:02:08 -0800

    Test camera intents and custom ImageView matcher

--- app/src/androidTest/java/com/codingwithmitch/espressouitestexamples/MainActivityTest.kt
+++ app/src/androidTest/java/com/codingwithmitch/espressouitestexamples/MainActivityTest.kt
@@ -1,23 +1,23 @@
 package com.codingwithmitch.espressouitestexamples
 
-import android.app.Activity.RESULT_OK
+import android.app.Activity
 import android.app.Instrumentation.ActivityResult
-import android.content.ContentResolver
 import android.content.Intent
-import android.content.res.Resources
-import android.net.Uri
+import android.graphics.BitmapFactory
+import android.os.Bundle
 import android.provider.MediaStore
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.intent.Intents.intended
 import androidx.test.espresso.intent.Intents.intending
-import androidx.test.espresso.intent.matcher.IntentMatchers.*
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasAction
 import androidx.test.espresso.intent.rule.IntentsTestRule
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
-import androidx.test.platform.app.InstrumentationRegistry
+import com.codingwithmitch.espressouitestexamples.ImageViewHasDrawableMatcher.hasDrawable
+import org.hamcrest.CoreMatchers.not
 import org.hamcrest.Matcher
-import org.hamcrest.Matchers.allOf
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -30,32 +30,30 @@ class MainActivityTest{
     val intentsTestRule = IntentsTestRule(MainActivity::class.java)
 
     @Test
-    fun  test_validateIntentSentToPickPackage() {
+    fun  test_cameraIntent() {
 
         // GIVEN
-        val expectedIntent: Matcher<Intent> = allOf(
-            hasAction(Intent.ACTION_PICK),
-            hasData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-        )
-        val activityResult = createGalleryPickActivityResultStub()
+        val activityResult = createImageCaptureActivityResultStub()
+        val expectedIntent: Matcher<Intent> = hasAction(MediaStore.ACTION_IMAGE_CAPTURE)
         intending(expectedIntent).respondWith(activityResult)
 
         // Execute and Verify
-        onView(withId(R.id.button_open_gallery)).perform(click())
+        onView(withId(R.id.image)).check(matches(not(hasDrawable())))
+        onView(withId(R.id.button_launch_camera)).perform(click())
         intended(expectedIntent)
+        onView(withId(R.id.image)).check(matches(hasDrawable()))
     }
 
-    private fun createGalleryPickActivityResultStub(): ActivityResult {
-        val resources: Resources = InstrumentationRegistry.getInstrumentation().context.resources
-        val imageUri = Uri.parse(
-            ContentResolver.SCHEME_ANDROID_RESOURCE + "://" +
-                    resources.getResourcePackageName(R.drawable.ic_launcher_background) + '/' +
-                    resources.getResourceTypeName(R.drawable.ic_launcher_background) + '/' +
-                    resources.getResourceEntryName(R.drawable.ic_launcher_background)
+    private fun createImageCaptureActivityResultStub(): ActivityResult? {
+        val bundle = Bundle()
+        bundle.putParcelable(
+            KEY_IMAGE_DATA, BitmapFactory.decodeResource(
+                intentsTestRule.getActivity().getResources(), R.drawable.ic_launcher_background
+            )
         )
-        val resultIntent = Intent()
-        resultIntent.setData(imageUri)
-        return ActivityResult(RESULT_OK, resultIntent)
+        val resultData = Intent()
+        resultData.putExtras(bundle)
+        return ActivityResult(Activity.RESULT_OK, resultData)
     }
 }
 
--- app/src/main/java/com/codingwithmitch/espressouitestexamples/MainActivity.kt
+++ app/src/main/java/com/codingwithmitch/espressouitestexamples/MainActivity.kt
@@ -2,25 +2,29 @@ package com.codingwithmitch.espressouitestexamples
 
 import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
+import androidx.annotation.VisibleForTesting
 import androidx.appcompat.app.AppCompatActivity
-import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
 
-const val GALLERY_REQUEST_CODE = 1234
+
+const val REQUEST_IMAGE_CAPTURE = 1234
+const val KEY_IMAGE_DATA = "data"
 
 class MainActivity : AppCompatActivity(){
 
     private val TAG: String = "AppDebug"
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button_open_gallery.setOnClickListener {
-            pickFromGallery()
+        button_launch_camera.setOnClickListener {
+            dispatchCameraIntent()
         }
     }
 
@@ -30,22 +34,23 @@ class MainActivity : AppCompatActivity(){
             Log.d(TAG, "RESULT_OK")
             when(requestCode){
 
-                GALLERY_REQUEST_CODE -> {
-                    Log.d(TAG, "GALLERY_REQUEST_CODE detected.")
-                    data?.data?.let { uri ->
-                        Log.d(TAG, "URI: $uri")
-                        Glide.with(this)
-                            .load(uri)
-                            .into(image)
+                REQUEST_IMAGE_CAPTURE -> {
+                    Log.d(TAG, "REQUEST_IMAGE_CAPTURE detected.")
+                    data?.extras.let{ extras ->
+                        if (extras == null || !extras.containsKey(KEY_IMAGE_DATA)) {
+                            return
+                        }
+                        val imageBitmap = extras[KEY_IMAGE_DATA] as Bitmap?
+                        image.setImageBitmap(imageBitmap)
                     }
                 }
             }
         }
     }
 
-    private fun pickFromGallery() {
-        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-        startActivityForResult(intent, GALLERY_REQUEST_CODE)
+    private fun dispatchCameraIntent() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
     }
 
 }
