fc960c02c1a10d593cb3e5cc271907ed96464a0a
Mitch Tabian <mitch@tabian.ca>
2019-11-29 10:40:31 -0800

    starting code

--- app/src/main/java/com/codingwithmitch/espressouitestexamples/MainActivity.kt
+++ app/src/main/java/com/codingwithmitch/espressouitestexamples/MainActivity.kt
@@ -1,10 +1,8 @@
-package com.codingwithmitch.espressouitestexamples.ui.main
+package com.codingwithmitch.espressouitestexamples
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.codingwithmitch.espressouitestexamples.R
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,10 +11,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button_next_activity.setOnClickListener {
-            val intent = Intent(this, SecondaryActivity::class.java)
-            startActivity(intent)
-        }
+
     }
 
 }
