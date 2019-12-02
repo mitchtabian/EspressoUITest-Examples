fbb966fe3c81ec144278a976eef3e84a744e5272
Mitch Tabian <mitch@tabian.ca>
2019-12-02 15:12:19 -0800

    No dependencies in simple example.

--- app/src/main/java/com/codingwithmitch/espressouitestexamples/factory/MovieFragmentFactory.kt
+++ app/src/main/java/com/codingwithmitch/espressouitestexamples/factory/MovieFragmentFactory.kt
@@ -2,16 +2,11 @@ package com.codingwithmitch.espressouitestexamples.factory
 
 
 import androidx.fragment.app.FragmentFactory
-import com.bumptech.glide.request.RequestOptions
-import com.codingwithmitch.espressouitestexamples.data.source.MoviesDataSource
 import com.codingwithmitch.espressouitestexamples.ui.movie.DirectorsFragment
 import com.codingwithmitch.espressouitestexamples.ui.movie.MovieDetailFragment
 import com.codingwithmitch.espressouitestexamples.ui.movie.StarActorsFragment
 
-class MovieFragmentFactory(
-    private val requestOptions: RequestOptions? = null,
-    private val moviesDataSource: MoviesDataSource? = null
-) : FragmentFactory(){
+class MovieFragmentFactory : FragmentFactory(){
 
     private val TAG: String = "AppDebug"
 
@@ -19,15 +14,8 @@ class MovieFragmentFactory(
 
         when(className){
 
-
             MovieDetailFragment::class.java.name -> {
-                if(requestOptions != null
-                    && moviesDataSource != null){
-                    MovieDetailFragment()
-                }
-                else{
-                    super.instantiate(classLoader, className)
-                }
+                MovieDetailFragment()
             }
 
             DirectorsFragment::class.java.name -> {
