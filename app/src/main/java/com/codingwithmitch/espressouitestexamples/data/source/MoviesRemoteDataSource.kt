7254d24b0f349d6560c79c96af569a2d24cb1d9b
mitch <mitch@tabian.ca>
2019-12-13 13:11:58 -0800

    remove unnecessary function

--- app/src/main/java/com/codingwithmitch/espressouitestexamples/data/source/MoviesRemoteDataSource.kt
+++ app/src/main/java/com/codingwithmitch/espressouitestexamples/data/source/MoviesRemoteDataSource.kt
@@ -22,26 +22,6 @@ class MoviesRemoteDataSource: MoviesDataSource {
     }
 
     private fun addMovie(
-        id: Int,
-        title: String,
-        image: String,
-        description: String,
-        directors: ArrayList<String>?,
-        star_actors: ArrayList<String>?
-    ){
-        val movie = Movie(
-            id = id,
-            title = title,
-            image = image,
-            description = description,
-            directors = directors,
-            star_actors = star_actors
-        )
-        MOVIES_REMOTE_DATA.put(id, movie)
-    }
-
-
-    private fun addMovie(
         movie: Movie
     ){
         MOVIES_REMOTE_DATA.put(movie.id, movie)
